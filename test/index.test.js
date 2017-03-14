a80066245a4cb5564feda4bffd2849908a93dd1f
Andiedie <zchangan@163.com>
2017-03-14 20:24:56 +0800

    init

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+/**
+* @Author: 周长安
+* @Date:   2017-03-14 14:00:09
+* @Email:  zchangan@163.com
+* @Last modified by:   周长安
+* @Last modified time: 2017-03-14 17:28:10
+*/
+const history = require('../lib/index');
+const router = require('./router.js');
+
+history({router});
